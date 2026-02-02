3e577d8e47b26298e9150286b34f4ef528f9d525
Antonio Innocente <antonioinnocente@Antonios-MacBook-Pro.local>
2026-02-02 19:36:07 +0000

    fix setup.ts

--- client/src/test/setup.ts
+++ client/src/test/setup.ts
@@ -7,3 +7,18 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+// ----------------------------------------------------
+// ResizeObserver polyfill for JSDOM (required by Radix UI)
+// ----------------------------------------------------
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+// Only add it if JSDOM hasn't already provided one
+if (!('ResizeObserver' in globalThis)) {
+  // @ts-expect-error - intentional test polyfill
+  globalThis.ResizeObserver = ResizeObserverMock;
+}
